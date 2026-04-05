0659d02a08eea72408edc55c2ff68bb73f0f5162
nikunjvarshney <nikunjvarshneya@gmail.com>
2026-04-05 17:40:57 +0530

    fix cors

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.options('*', cors());
 app.use(express.json()); 
 
 
