2d36948dd5bd006ca31ce2ce2a5f6704d958528a
nikunjvarshney <nikunjvarshneya@gmail.com>
2026-04-05 18:34:47 +0530

    backend update

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 
 
 app.use(cors({
-  origin: '*',
+   origin: [
+    'http://localhost:5173',
+    'https://billing-project-frontend.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
